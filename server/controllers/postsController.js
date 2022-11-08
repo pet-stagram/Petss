ee72bb98dbfc8a1b4d1167cab9f8ea7cffa9a297
nimoh <spakers38@gmail.com>
2022-11-08 12:43:53 +0900

    git ignore 수정

--- server/controllers/postsController.js
+++ server/controllers/postsController.js
@@ -1,5 +1,7 @@
+const service = require("../services/postsService");
+
 module.exports = {
     getPosts : (req, res) =>{
-        
+        service.selectPostsAll;
     }
 }
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -3,12 +3,7 @@ const router = express.Router();
 const controller = require("../controllers/postsController.js");
 
 router.get("/",controller.getPosts);
-// router.get("/:id",controller.getPostsId);
 
-// router.post("/",controller.postPosts);
-
-// router.put("/like",controller.putLike);
-// router.put("/unlike",controller)
 
 
 module.exports = router;
