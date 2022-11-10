f407ee5d767a1ac68e5ba4b27334658740dc99af
mmlyujin <109034378+mmlyujin@users.noreply.github.com>
2022-11-10 14:49:04 +0900

    로그인 페이지 수정 중(로고 미완성)

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -10,6 +10,7 @@ function Login() {
           <div class="mainBox">
             <div class="logoBox">
               <div class="logo">
+                {/* 로고 바꾸고 이미지 화질 개선하기 */}
                 <div>
                   <img src={Logo} alt="Logo" />
                 </div>
@@ -21,14 +22,25 @@ function Login() {
                   <strong>Login</strong>
                 </h2>
                 <div>
-                  <input type="text" placeholder="아이디" class="id" />
+                  <input
+                    type="text"
+                    placeholder="이메일"
+                    class="email"
+                    name="email"
+                  />
                 </div>
                 <div>
-                  <input type="password" placeholder="비밀번호" class="pw" />
+                  <input
+                    type="password"
+                    placeholder="비밀번호"
+                    class="pw"
+                    name="pw"
+                  />
                 </div>
                 <input type="submit" value="로그인" class="btn" />
                 <div class="findPw">
-                  <a href="#">비밀번호를 잊으셨나요?</a>
+                  {/* 아이디 / 비밀번호 찾는 팝업창 만들기 */}
+                  <a href="#">아이디 혹은 비밀번호를 잊으셨나요?</a>
                 </div>
                 <div class="mvReg">
                   <a href="#">계정이 없으신가요? 가입하기</a>
