0396b80f9696c89f79320960e1c13c11cf5c8885
nimoh <spakers38@gmail.com>
2022-11-16 23:37:03 +0900

    채팅방 가져오기, 채팅 보내기 구현 완료

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -3,10 +3,32 @@ const service = require("../services/chatService");
 module.exports ={
     getChatRooms : async (req, res)=>{
         const currentUser = 1 // 현재 세션 정보
-        await service.selectChatRooms(currentUser);
+        try{
+            const selectResult = await service.selectChatRooms(currentUser);
+            res.status(201).json(selectResult);
+        }catch(err){
+            res.sendStatus(500);
+        }
+    },    
+    getMessage : async (req, res)=>{
+        const messageDto = {
+            receiver : 2,//req.query.receiver
+            sender : 1 // 세션아이디
+        };
+        await service.selectMessages(messageDto);
     },
     postMessage : async (req ,res) =>{
-        const currentUser = 1;
-        await service.createMessages(currentUser);
+        const messageDto = {
+            currentUser : 1,
+            content : req.body.content
+        } 
+        try{
+            await service.createMessages(messageDto);
+            res.sendStatus(201);
+        }
+        catch(err){
+            res.sendStatus(500);
+            throw err;
+        }
     }
 }
--- server/services/chatService.js
+++ server/services/chatService.js
@@ -1,47 +1,62 @@
-const {
-    Conversation,
-    Message
-} = require("../sequelize/models/index");
+const { Conversation, Message } = require("../sequelize/models/index");
 const { Op } = require("sequelize");
 const sequelize = require("sequelize");
 const { uploadProfileImage } = require("../module/firebase");
 const db = require("../sequelize/models/index");
 const usersController = require("../controllers/usersController");
-module.exports ={
-    selectChatRooms: async (currentUser) =>{
-
-    },
-    createMessages : async (currentUser) =>{
-       let conversation = await Conversation.findOne({
-        where:{
-            user1: {[Op.or]:[currentUser,2]},
-            user2: {[Op.or]:[currentUser,2]}
+module.exports = {
+    selectChatRooms: async (currentUser) => {
+        try {
+            const conversations = await Conversation.findAll({
+                where: {
+                    [Op.or]: [{ user1: currentUser }, { user2: currentUser }],
+                },
+                raw: true,
+            });
+            return conversations;
+        } catch (err) {
+            throw err;
         }
-       });
-
-       if(!conversation){
-        conversation = await Conversation.create({
-            user1: currentUser,
-            user2: 2,
-            last_chat : "안녕 ㅋㅋ"
-        });
-       }
-       const newMessage = await Message.create({
-        senderId: currentUser,
-        receiverId : 2,
-        content: "안녕 ㅋㅋ",
-        conversationId: conversation.id
-       });
-
-       const updateLastChat = await Conversation.update(
-        {lastChat : "안녕 ㅋㅋ"},
-            {
-                where:{
-                    id: conversation.id
-                 }
+    },
+    selectMessages: async (messageDto)=>{
+        const { receiver, sender } = messageDto;
+        const conversation = await Conversation.findOne({
+            where: {
+                user1: { [Op.or]: [receiver, sender] },
+                user2: { [Op.or]: [receiver, sender] }
             }
-         );
-         console.log(updateLastChat);
-    
-}
-}
+        });
+    },
+    createMessages: async (messageDto) => {
+        const { currentUser, content } = messageDto;
+        /* 현재 유저, 상대방, 채팅 내용 가져와야함 */
+        let conversation = await Conversation.findOne({
+            where: {
+                user1: { [Op.or]: [currentUser, 2] },
+                user2: { [Op.or]: [currentUser, 2] },
+            },
+        });
+        if (!conversation) {
+            conversation = await Conversation.create({
+                user1: currentUser,
+                user2: 2,
+                last_chat: content,
+            });
+        }
+        const newMessage = await Message.create({
+            senderId: currentUser,
+            receiverId: 2,
+            content: content,
+            conversationId: conversation.id,
+            sendAt: Date.now(),
+        });
+        const updateLastChat = await Conversation.update(
+            { lastChat: content },
+            {
+                where: {
+                    id: conversation.id,
+                },
+            }
+        );
+    },
+};
