943ff51bab34a9389341fca6ffd9a26a8ef6c305
nimoh <spakers38@gmail.com>
2022-11-17 12:29:29 +0900

    commit for merge

--- client/src/components/feed/layout/Side.js
+++ client/src/components/feed/layout/Side.js
@@ -1,34 +1,43 @@
 import React from 'react'
 import "../../css/side.css"
-import { useEffect } from 'react';
-import { axios } from 'axios';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 
 
 const Side = () => {
   
+  const [conversations, setConversations] = useState([]);
+
   useEffect(()=>{
-    axios.get("/chat/rooms")
-    .then((data)=>{
-      console.log(data);
-    })
-    .catch(()=>{})
-  },[])
+    axios.get("api/chat/rooms").then((result)=>{
+      result.data.map((data)=>{
+        setConversations([...conversations, data]);
+      });
+    });
+  },[]);
+  console.log(conversations);
   
   return (
-    <>
+    <div className="side">
     <h2>Message</h2>
     <div>
-        <div className="messageUserInfo">
-            <span className="messageUserImage"></span>
-            <span>뭉치</span>
-        </div>
-        <div className="messageUserInfo">
-            <span className="messageUserImage2"></span>
-            <span>토리 유딘</span>
-        </div>
+       {
+        conversations.map((conversation, index)=>{
+          const friend = conversation.User1===null?conversation.User2:conversation.User1;
+          return(
+        <div className="messageUserInfo" key={index}>
+          <img src={friend.image} alt="친구 프로필사진" className="friendImg"/>
+          <span>{friend.name}</span>
+          안녕
+          </div>
+          );
+          
+        })
+       }
         <button className='testBtn'>Message</button>
     </div>
-    </>
+    </div>
   )
 }
 
