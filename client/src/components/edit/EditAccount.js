9f71196b8a836de42bd9c698c27bb66b6896be78
mmlyujin <109034378+mmlyujin@users.noreply.github.com>
2022-11-24 10:31:19 +0900

    설정화면 디자인 중

--- client/src/components/edit/EditAccount.js
+++ client/src/components/edit/EditAccount.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../edit/editAccount.css";
 import Navbar from "../feed/layout/Navbar";
+import Profile from "../../images/1.jpg";
 
 function EditProfile() {
   //axios.get()으로 유저 정보 가져와서 input칸에 반영시키고 제출할 때 유저정보 수정될 수 있게(post)
@@ -14,25 +15,29 @@ function EditProfile() {
           <div className="navbarWrap">
             <Navbar />
           </div>
-          <div className="editProfileWrap">
-            <div className="editProfileBox">
-              <div className="rowPhotoChange">
+          <div className="editAccountWrap">
+            <div className="editAccountBox">
+              <div className="editRow" id="rowTop">
                 <div className="left">
                   <div>
-                    <button className="">
-                      <img alt="내 프로필 사진바꾸기" className="" src=""></img>
+                    <button className="editPhotoBtn">
+                      {/* <img
+                        alt="본인 프로필 사진"
+                        className=""
+                        src={Profile}
+                      ></img> */}
                     </button>
                   </div>
                 </div>
                 <div className="right">
-                  <h1>로그인 한 유저 활동명 띄우기</h1>
+                  <h1>닉네임자리</h1>
                   <button className="" type="button">
-                    프로필 사진 바꾸기
+                    프로필 사진 바꾸기(누르면 모달 띄우기)
                   </button>
                 </div>
               </div>
               <form className="" method="post">
-                <div className="rowName">
+                <div className="editRow">
                   <aside>
                     <label>이름</label>
                   </aside>
@@ -47,7 +52,7 @@ function EditProfile() {
                     ></input>
                   </div>
                 </div>
-                <div className="rowNick">
+                <div className="editRow">
                   <aside>
                     <label>활동명</label>
                   </aside>
@@ -58,21 +63,25 @@ function EditProfile() {
                       aria-required="true"
                       name=""
                       value=""
+                      placeholder="활동명"
                     ></input>
                   </div>
                 </div>
-                <div className="rowIntro">
+                <div className="editRow" id="rowIntroduce">
                   <aside>
                     <label>소개</label>
                   </aside>
                   <div>
                     <div>
-                      <textarea className=""></textarea>
+                      <textarea
+                        maxLength="150"
+                        placeholder="텍스트를 입력하세요"
+                      ></textarea>
                       <div>글자적으면 숫자 올라가고 텍스트 수 제한걸기</div>
                     </div>
                   </div>
                 </div>
-                <div className="rowEmail">
+                <div className="editRow" id="editRowEmail">
                   <aside>
                     <label>이메일</label>
                   </aside>
@@ -86,7 +95,7 @@ function EditProfile() {
                     ></input>
                   </div>
                 </div>
-                <div className="rowChkEmail">
+                <div className="editRow">
                   <aside>
                     <label></label>
                   </aside>
@@ -94,7 +103,7 @@ function EditProfile() {
                     <button type="button">이메일 확인</button>
                   </div>
                 </div>
-                <div className="rowPhone">
+                <div className="editRow">
                   <aside>
                     <label>전화번호</label>
                   </aside>
@@ -102,14 +111,59 @@ function EditProfile() {
                     <input
                       className=""
                       type="text"
-                      aria-required="false"
+                      aria-required="true"
                       placeholder="전화번호"
                       name=""
                       value=""
                     ></input>
                   </div>
                 </div>
-                <div className="rowBtn">
+                <div className="editRow">
+                  <aside>
+                    <label>이전 비밀번호</label>
+                  </aside>
+                  <div>
+                    <input
+                      className=""
+                      type="text"
+                      aria-required="true"
+                      placeholder="기존 비밀번호 띄울 예정"
+                      name=""
+                      value=""
+                    ></input>
+                  </div>
+                </div>
+                <div className="editRow">
+                  <aside>
+                    <label>새 비밀번호</label>
+                  </aside>
+                  <div>
+                    <input
+                      className=""
+                      type="text"
+                      aria-required="false"
+                      placeholder=""
+                      name=""
+                      value=""
+                    ></input>
+                  </div>
+                </div>
+                <div className="editRow">
+                  <aside>
+                    <label>새 비밀번호 확인</label>
+                  </aside>
+                  <div>
+                    <input
+                      className=""
+                      type="text"
+                      aria-required="false"
+                      placeholder=""
+                      name=""
+                      value=""
+                    ></input>
+                  </div>
+                </div>
+                <div className="editRow">
                   <aside>
                     <label></label>
                   </aside>
