07de2cd525c3c8dfe2b35991c88b56561754b246
mmlyujin <109034378+mmlyujin@users.noreply.github.com>
2022-11-25 20:26:03 +0900

    푸터  제작중

--- client/src/components/edit/EditAccount.js
+++ client/src/components/edit/EditAccount.js
@@ -182,6 +182,21 @@ function EditProfile() {
             </div>
           </div>
         </div>
+        <footer>
+          <ul>
+            <li>petss</li>
+            <li>소개</li>
+            <li>채용 정보</li>
+            <li>도움말</li>
+            <li>개인정보처리방침</li>
+            <li>약관</li>
+            <li>해시태그</li>
+            <li>연락처 업로드 & 비사용자</li>
+          </ul>
+          <div>
+            <p>©2022 PETSS</p>
+          </div>
+        </footer>
       </body>
     </div>
   );
