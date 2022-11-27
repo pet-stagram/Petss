8e4aa25b6b4cd25280378a3dcbb03cf56963fa6a
iamyewon <101702107+iamyewon@users.noreply.github.com>
2022-11-27 18:29:11 +0900

    파일병합

--- client/src/components/feed/pages/MainFeed.js
+++ client/src/components/feed/pages/MainFeed.js
@@ -58,11 +58,12 @@ const MainFeed = () => {
   return (
     
         
-            // {(posts === null? <div>1</div> : <div>2</div>)}
+           
         
     
       <main className='mainFeedWrap'>
-            {
+        {posts === ""? <div>No Data</div> : 
+            
             isLoading &&
             posts.map((post)=>{
                 return(
@@ -91,7 +92,7 @@ const MainFeed = () => {
                             <button><img src={reply} alt="like" className="like"/></button>
                             <button><img src={message} alt="like" className="like"/></button>
                         </div>
-                        <p className='likeCount'>좋아요 {post.heart_count} 개  </p>
+                        <p className='likeCount'>좋아요 {post.Hearts.length} 개  </p>
                         <div className="postContent"> 
                             {post.content}
                         </div>
@@ -100,7 +101,8 @@ const MainFeed = () => {
                 </div>
                 );
             })
-            }
+          }
+        
            
         </main>
   )
