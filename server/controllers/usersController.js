0958e3b74d4d8d69d62075c7cb69535a58435e55
nimoh <spakers38@gmail.com>
2022-12-05 11:43:34 +0900

    for merge

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,147 +1,144 @@
 const service = require("../services/usersService");
 
 module.exports = {
-    getMe : async (req, res)=>{
+    getMe: async (req, res) => {
         const currentUser = req.session.u_id;
-        if(!currentUser){
+        if (!currentUser) {
             res.sendStatus(401);
-        }else{
-            try{
+        } else {
+            try {
                 const result = await service.selectUser(currentUser);
                 res.json(result);
-            }catch(err){
+            } catch (err) {
                 res.sendStatus(400);
                 console.error(err);
             }
         }
     },
-    getUser: async (req, res)=>{
+    getUser: async (req, res) => {
         const currentUser = req.session.u_id;
-        if(false){
+        if (false) {
             res.sendStatus(401);
-        }else{
-           const userId = req.params.userId;
-        try{
-            const result = await service.selectUser(userId);
-            res.status(200).json(result);
-        }catch(err){
-            console.error(err);
-            res.sendStatus(400);
-        }  
-        }
-       
-    },
-    putUserInfo : async (req, res)=>{
-        const currentUser = req.session.u_id;
-        if(!currentUser){
-            res.sendStatus(401);
-        }else{
-            const userDto = {
-                id : currentUser,
-                name : req.body.name,
-                nick : req.body.nick,
-                password : req.body.pw,
-                selfIntro : req.body.selfIntro
+        } else {
+            const userId = req.params.userId;
+            try {
+                const result = await service.selectUser(userId);
+                res.status(200).json(result);
+            } catch (err) {
+                console.error(err);
+                res.sendStatus(400);
             }
-                try{
-                    await service.updateUserInfo(userDto);
-                    res.sendStatus(201);
-                }catch(err){
-                    res.sendStatus(400);
-                    console.error(err);
-                }
         }
-        
     },
-    postUserImage : async (req, res)=>{
+    putUserInfo: async (req, res) => {
+        const currentUser = req.session.u_id;
+        if (!currentUser) {
+            res.sendStatus(401);
+        } else {
+            const userDto = {
+                id: currentUser,
+                name: req.body.name,
+                nick: req.body.nick,
+                password: req.body.pw,
+                selfIntro: req.body.selfIntro,
+            };
+            try {
+                await service.updateUserInfo(userDto);
+                res.sendStatus(201);
+            } catch (err) {
+                res.sendStatus(400);
+                console.error(err);
+            }
+        }
+    },
+    postUserImage: async (req, res) => {
         const currentUser = req.session.u_id;
         const userDto = {
-            id : currentUser, // 현재 세션 유저
+            id: currentUser, // 현재 세션 유저
             file: req.file.path,
-            isBasic: false
-        }
-        if(!currentUser){
+            isBasic: false,
+        };
+        if (!currentUser) {
             res.sendStatus(401);
-        }else if(isNaN(req.params.userId)){
+        } else if (isNaN(req.params.userId)) {
             res.sendStatus(404);
-        }else if(userDto.file === undefined){
+        } else if (userDto.file === undefined) {
             res.sendStatus(400);
-        } else{   
-                try{
-                    // await service.
-                    await service.updateUserImage(userDto);
-                    res.sendStatus(201);
-                }catch(err){
-                    console.error(err);
-                    res.sendStatus(400);
-                }
-            
-        }
-        
-
-    },
-    postUserImageBasic : async (req, res)=>{
-        const currentUser = req.session.u_id;
-        if(!currentUser){
-            res.sendStatus(401);
-        }else if(isNaN(req.params.userId)){
-            res.sendStatus(404);
-        }else{
-            const userDto = {
-                id : currentUser,
-                file: "public/images/basic_profile.jpeg",
-                isBasic: true
-            }
-            try{
+        } else {
+            try {
+                // await service.
                 await service.updateUserImage(userDto);
                 res.sendStatus(201);
-            }catch(err){
+            } catch (err) {
                 console.error(err);
                 res.sendStatus(400);
             }
         }
     },
-    putFollow : async (req, res) => {
+    postUserImageBasic: async (req, res) => {
         const currentUser = req.session.u_id;
-        if(!currentUser){
+        if (!currentUser) {
             res.sendStatus(401);
-        }else{
-            const followDto = {
-            currentUser,
-            profileUser : isNaN(req.body.userId)?req.body.userId:parseInt(req.body.userId)
-        };
-        try{
-            const result = await service.updateFollow(followDto);
-            if(result==="Bad Request"){
-                res.sendStatus(400);
-            }else if(result ==="Created"){
+        } else if (isNaN(req.params.userId)) {
+            res.sendStatus(404);
+        } else {
+            const userDto = {
+                id: currentUser,
+                file: "public/images/basic_profile.jpeg",
+                isBasic: true,
+            };
+            try {
+                await service.updateUserImage(userDto);
                 res.sendStatus(201);
-            }else{
-                res.sendStatus(204);
+            } catch (err) {
+                console.error(err);
+                res.sendStatus(400);
             }
-        }catch(err){
-            res.sendStatus(400);
-            console.error(err);
-        }
         }
     },
-    postInvoice : async (req, res) => {
+    putFollow: async (req, res) => {
         const currentUser = req.session.u_id;
-        if(!currentUser){
+        if (!currentUser) {
             res.sendStatus(401);
-        }else{
-            const invoiceDto = {
-                title : req.body.title,
-                content : req.body.content,
-                userId : currentUser
+        } else {
+            const followDto = {
+                currentUser,
+                profileUser: isNaN(req.body.userId)
+                    ? req.body.userId
+                    : parseInt(req.body.userId),
             };
-            try{
+            try {
+                const result = await service.updateFollow(followDto);
+                if (result === "Bad Request") {
+                    res.sendStatus(400);
+                } else if (result === "Created") {
+                    res.sendStatus(201);
+                } else {
+                    res.sendStatus(204);
+                }
+            } catch (err) {
+                res.sendStatus(400);
+                console.error(err);
+            }
+        }
+    },
+    postInvoice: async (req, res) => {
+        const currentUser = req.session.u_id;
+        if (!currentUser) {
+            res.sendStatus(401);
+        } else {
+            const invoiceDto = {
+                title: req.body.title,
+                content: req.body.content,
+                userId: currentUser,
+            };
+            try {
                 await service.insertInvoice(invoiceDto);
                 res.sendStatus(201);
-            }catch(err){
+            } catch (err) {
                 console.error(err);
                 res.sendStatus(400);
             }
         }
-    }
-}
+    },
+};
